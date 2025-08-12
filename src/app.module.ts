f56bfc73122a73ff3f7e46427f9c158c59b8025c
Ahmed Gamal <ahmadmoshaa3@gmail.com>
2025-08-12 16:46:52 +0300

    create core modules structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getDatabaseConfig } from './config/database.config';
+import { BooksModule } from './books/books.module';
+import { BorrowersModule } from './borrowers/borrowers.module';
+import { BorrowingModule } from './borrowing/borrowing.module';
+import { ReportsModule } from './reports/reports.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getDatabaseConfig,
+      inject: [ConfigService],
+    }),
+    BooksModule,
+    BorrowersModule,
+    BorrowingModule,
+    ReportsModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
